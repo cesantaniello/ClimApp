0fd95d412ad881c2b9f35946e9a373e4605847c1
Administrator <admin@example.com>
2021-10-05 11:16:13 +0200

    1. Crea funcion buscarClima

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.addEventListener('load', () => {
+    formulario.addEventListener('submit', buscarClima);
+});
+
+function buscarClima(e) {
+    e.preventDefault();
+}
