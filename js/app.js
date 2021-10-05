8c273a7f5a0029b1815b4d7dd177062f0cd4dcc7
Administrator <admin@example.com>
2021-10-05 11:33:14 +0200

    2. Valida formulario

--- js/app.js
+++ js/app.js
@@ -8,4 +8,39 @@ window.addEventListener('load', () => {
 
 function buscarClima(e) {
     e.preventDefault();
+
+    // Validar
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    if (ciudad === '' || pais === '') {
+        //Hubo un error
+        mostrarError('Ambos campos son obligatorios');
+        return;
+    }
+}
+
+function mostrarError(mensaje) {
+    const alerta = document.querySelector('.bg-red-100');
+
+    if(!alerta) {
+
+        // Crear una alerta
+        const alerta = document.createElement('div');
+        
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+        
+        alerta.innerHTML = `
+        <strong class="font-bold">Error!</strong>
+        <span class="block">${mensaje}</span>
+        `;
+        
+        container.appendChild(alerta);
+        
+        // Se elimina la alerta despues de 5 segundos
+        setTimeout(() => {
+            alerta.remove();
+        }, 5000);
+    }
+
 }
